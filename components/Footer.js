300c47c97e934d1c3f9d96414ee8d31b3770c0cf
ariverak <jrivera.v@outlook.com>
2021-09-02 21:19:46 -0400

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <code className='layout-footer'>
+      Movil : 976186028 Mail : info@jamaadministraciones.cl
+    </code>
+  );
+};
+
+Footer.propTypes = {};
+
+export default Footer;
