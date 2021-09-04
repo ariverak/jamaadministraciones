ee0d685d6b25b5a85a063281a5a516e049eea9b3
Santos Castillo <santosdummont@gmail.com>
2021-09-03 22:36:44 -0400

    servicios

--- components/Features.js
+++ components/Features.js
@@ -5,11 +5,11 @@ function Features() {
       <section className='content-inner bg-white'>
         <div className='container'>
           <div className='section-head style-3 text-center'>
-            <h2 className='title'>Our Fetures</h2>
+            <h2 className='title'>Nuestros servicios</h2>
             <div className='dlab-separator style-2 bg-primary'></div>
           </div>
-          <div className='row align-items-center about-wraper-2'>
-            <div className='col-lg-4'>
+          <div className='row align-items-top about-wraper-2'>
+            <div className='col-lg-6'>
               <div className='row'>
                 <div
                   className='col-lg-12 col-md-6 wow fadeInUp'
@@ -22,17 +22,30 @@ function Features() {
                         href='javascript:void(0);'
                         className='icon-cell text-white'
                       >
-                        <i className='flaticon-idea'></i>
+                        <i className='flaticon-dollar'></i>
                       </a>
                     </div>
                     <div className='icon-content'>
                       <h4 className='dlab-tilte text-capitalize'>
-                        Idea & Analysis
+                        Remuneraciones
                       </h4>
-                      <p>
-                        Praesent tincidunt congue est ut hendrerit. Pellentesque
-                        et eros sit amet ipsum venenatis.
-                      </p>
+                      <ul className='list-check style-1 primary m-b30 align-items-top'>
+                        <li>
+                          Revisión de Contratos, Feriados y Documentación
+                          particular de cada trabajador Realizar remuneraciones
+                          del personal
+                        </li>
+                        <li>
+                          Cancelar remuneraciones mensuales Elaboración y pago
+                          de Imposiciones mensuales
+                        </li>
+                        <li>Coordinar y registrar vacaciones del personal</li>
+                        <li>
+                          Representar al Comité ante Inspección del Trabajo,
+                          AFP, Isapres, Fonasa y otras instituciones
+                          relacionadas.
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -47,31 +60,54 @@ function Features() {
                         href='javascript:void(0);'
                         className='icon-cell text-white'
                       >
-                        <i className='flaticon-vector'></i>
+                        <i className='flaticon-chart'></i>
                       </a>
                     </div>
                     <div className='icon-content'>
-                      <h4 className='dlab-tilte text-capitalize'>Designing</h4>
-                      <p>
-                        Praesent tincidunt congue est ut hendrerit. Pellentesque
-                        et eros sit amet ipsum venenatis.
-                      </p>
+                      <h4 className='dlab-tilte text-capitalize'>
+                        Informes de gestión
+                      </h4>
+                      <ul className='list-check style-1 primary m-b30'>
+                        <li>Libro Diario de Movimientos</li>
+                        <li>Cartola Bancaria Mensual</li>
+                        <li>Conciliación Bancaria Mensual</li>
+                        <li>
+                          Estadísticas de Ingresos y Gastos mensuales y anuales
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div
-              className='col-lg-4 wow zoomIn'
-              data-wow-duration='2s'
-              data-wow-delay='0.2s'
-            >
-              <div className='dz-media text-center m-b30'>
-                <img src='images/about/img7.png' className='move-1' alt='' />
-              </div>
-            </div>
-            <div className='col-lg-4'>
+
+            <div className='col-lg-6'>
               <div className='row'>
+                <div
+                  className='col-lg-12 col-md-6 wow fadeInUp'
+                  data-wow-duration='2s'
+                  data-wow-delay='0.4s'
+                >
+                  <div className='icon-bx-wraper left m-b50 icon-up'>
+                    <div className='icon-bx-sm radius gradient-sec shadow-primary m-b20'>
+                      <a
+                        href='javascript:void(0);'
+                        className='icon-cell text-white'
+                      >
+                        <i className='flaticon-user'></i>
+                      </a>
+                    </div>
+                    <div className='icon-content'>
+                      <h4 className='dlab-tilte text-capitalize'>
+                        Proveedores
+                      </h4>
+                      <ul className='list-check style-1 primary m-b30'>
+                        <li>Registro de facturas y/o boletas por servicios</li>
+                        <li>Pago de trabajos efectuados</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
                 <div
                   className='col-lg-12 col-md-6 wow fadeInUp'
                   data-wow-duration='2s'
@@ -83,17 +119,18 @@ function Features() {
                         href='javascript:void(0);'
                         className='icon-cell text-white'
                       >
-                        <i className='flaticon-coding'></i>
+                        <i className='flaticon-sketch'></i>
                       </a>
                     </div>
                     <div className='icon-content'>
                       <h4 className='dlab-tilte text-capitalize'>
-                        Development
+                        Gastos Comunes
                       </h4>
-                      <p>
-                        Praesent tincidunt congue est ut hendrerit. Pellentesque
-                        et eros sit amet ipsum venenatis.
-                      </p>
+
+                      <ul className='list-check style-1 primary m-b30'>
+                        <li>Realizar Gastos Comunes mensuales</li>
+                        <li>Cobranza de Gastos Comunes</li>
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -108,17 +145,22 @@ function Features() {
                         href='javascript:void(0);'
                         className='icon-cell text-white'
                       >
-                        <i className='flaticon-rocket'></i>
+                        <i className='flaticon-check'></i>
                       </a>
                     </div>
                     <div className='icon-content'>
-                      <h4 className='dlab-tilte text-capitalize'>
-                        Testing & Lunching
-                      </h4>
-                      <p>
-                        Praesent tincidunt congue est ut hendrerit. Pellentesque
-                        et eros sit amet ipsum venenatis.
-                      </p>
+                      <h4 className='dlab-tilte text-capitalize'>Otros</h4>
+
+                      <ul className='list-check style-1 primary m-b30'>
+                        <li>Revisión y control Seguros Edificios</li>
+                        <li>
+                          Revisión equipamiento de seguridad (Alarmas,
+                          extintores, cámaras de seguridad, etc.)
+                        </li>
+                        <li>Atención Residentes</li>
+                        <li>Normativas en uso de Espacios Comunes</li>
+                        <li>Controles de acceso</li>
+                      </ul>
                     </div>
                   </div>
                 </div>
