1ef427eab36c0155874d852d083f7836afcca0dc
ariverak <jrivera.v@outlook.com>
2021-09-06 18:06:24 -0300

    fix mail

--- public/scripts/mail.php
+++ public/scripts/mail.php
@@ -2,6 +2,7 @@
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $crlf = "\r\n";
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
 		$name = str_replace(array("\r","\n"),array(" "," "),$name);
@@ -27,17 +28,19 @@
         $subject = "Nuevo contacto de $name";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "Nombre: $name\n";
         $email_content .= "Email: $email\n";
         $email_content .= "Teléfono: $phone\n";
         $email_content .= "Ciudad: $city\n";
         $email_content .= "Dirección: $address\n";
-        $email_content .= "Message:$message\n";
+        $email_content .= "Mensaje:$message\n";
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $headers =  "From:  $name <$email>".$crlf.
+                    'Content-Type: text/plain; charset=UTF-8'.$crlf.
+                    'X-Mailer: PHP/' . phpversion();
 
         // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+        if (mail($recipient, $subject, $email_content, $headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
